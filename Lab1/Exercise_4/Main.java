3b845e24d06af66ed63a3c99c4a90c0cbf1bcd78
Bence Molnar <molnarbence2014@gmail.com>
2024-10-13 20:39:56 +0300

    finished last function with the best combined price + added some short function descriptions

--- Lab1/Exercise_4/Main.java
+++ Lab1/Exercise_4/Main.java
@@ -1,7 +1,12 @@
 package MAPLabor.Lab1.Exercise_4;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        test1();
+        test2();
+        test3();
         Keyboard k1 = new Keyboard(15);
         Keyboard k2 = new Keyboard(80);
         Keyboard k3 = new Keyboard(39);
@@ -14,8 +19,39 @@ public class Main {
         System.out.println("Cheapest Keyboard: " + getCheapestKeyboard());
         System.out.println("Most Expensive Tool: " + getMostExpensiveTool());
         System.out.println("Most Expensive USB from 30$: " + getMostExpensiveUSB(30));
+        System.out.println("Most Expensive Setup from 50$: " + bestCombination(50));
     }
 
+    //? just a few lines having fun with assertions
+    public static void test1() {
+        Keyboard k1 = new Keyboard(60);
+        USB u1 = new USB(8);
+        USB u2 = new USB(12);
+
+        int result = bestCombination(60);
+        assert result == -1 : "Assertion failed";
+        System.out.println("Assertion working!");
+    }
+
+    public static void test2() {
+        Keyboard k1 = new Keyboard(40);
+
+        int result = bestCombination(60);
+        assert result == 52 : "Assertion failed";
+        System.out.println("Assertion working!");
+    }
+
+    public static void test3() {
+        Keyboard k1 = new Keyboard(50);
+
+        int result = bestCombination(60);
+        assert result == 58 : "Assertion failed";
+        System.out.println("Assertion working!");
+    }
+
+
+    
+    //? returns the price of the cheapest Keyboard
     public static int getCheapestKeyboard() {
         int min = 10000;
         for (Keyboard keyboard : Keyboard.getAllKeyboards()) {
@@ -26,6 +62,7 @@ public class Main {
         return min;
     }
 
+    //? returns the most expensive tool
     public static int getMostExpensiveTool() {
         int max = 0;
         for (USB usb : USB.getAllUSBs()) {
@@ -40,6 +77,7 @@ public class Main {
         return max;
     }
 
+    //? returns the most expensive USB within the budget
     public static int getMostExpensiveUSB(int budget) {
         int max = 0;
         for (USB usb : USB.getAllUSBs()) {
@@ -47,5 +85,22 @@ public class Main {
                 max = usb.getPrice();
         }
         return max;
+    }
+
+    //? checks for the most expensive combination within the budget. returns -1 if inexistent
+    public static int bestCombination(int budget) {
+        int price = -1;
+        List<Keyboard> keyboards = Keyboard.getAllKeyboards();
+        List<USB> usbs = USB.getAllUSBs();
+
+        for (int i = 0; i < keyboards.size(); i++) {
+            for (int j = 0; j < usbs.size(); j++) {
+                int combination = keyboards.get(i).getPrice() + usbs.get(j).getPrice();
+                if (combination <= budget && combination > price) {
+                    price = combination;
+                }
+            }
+        }
+        return price;
     }
 }
